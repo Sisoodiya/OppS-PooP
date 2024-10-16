8f8fd45e54de4bc38a31bb683d52664b158bd559
Sanjay Singh <singhsanjay32220@gmail.com>
2024-10-16 08:09:52 +0530

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("BMW" , 2024 ,"Blue" , False )
+car2 = Car("Corvette" , 2024 ,"Red" , True)
+car3 = Car("Mustang" , 2024 ,"White" , False)
+
+car1.drive()
+car2.stop()
+
+car3.describe()
